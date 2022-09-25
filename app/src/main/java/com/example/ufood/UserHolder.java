90fa47dc50880c0e52ea39deb918a1f3f17ea052
abhik <abhikchowdhury.20@nshm.edu.in>
2022-09-25 20:17:49 +0530

    This app will going help people who want to help others
    Update on 23sept with two new activity
    new on 24th splash gone
    CircleTransform new java class to make the image circular
    Firebase Two branch again

---
+++ app/src/main/java/com/example/ufood/UserHolder.java
@@ -0,0 +1,69 @@
+package com.example.ufood;
+
+public class UserHolder {
+    public String user_name;
+    public String email;
+    public String phone;
+    public String password;
+    public String profile_pic;
+    public String user_type;
+    public UserHolder(){
+
+    }
+
+    public UserHolder(String user_name, String email, String phone, String password, String profile_pic, String user_type) {
+        this.user_name = user_name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.profile_pic = profile_pic;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfile_pic() {
+        return profile_pic;
+    }
+
+    public void setProfile_pic(String profile_pic) {
+        this.profile_pic = profile_pic;
+    }
+
+    public String getUser_type() {
+        return user_type;
+    }
+
+    public void setUser_type(String user_type) {
+        this.user_type = user_type;
+    }
+}
--- app/src/main/java/com/example/ufood/user_Registration.java
+++ app/src/main/java/com/example/ufood/user_Registration.java
@@ -1,41 +1,73 @@
 package com.example.ufood;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
-import java.util.Objects;
+import java.util.ArrayList;
 
 
 public class user_Registration extends AppCompatActivity {
     ImageView profile_pic;
     ImageView add_image;
+    Spinner spinner;
+    ProgressBar progressBar;
+    EditText Full_name,Email,Phone,Password,Confirm_password;
+    Uri uri;
+    private final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registration);
-         profile_pic = findViewById(R.id.profile_pic);
-         add_image = findViewById(R.id.add_image);
-        Spinner spinner = findViewById(R.id.spinner);
-        ArrayAdapter <CharSequence> userType = ArrayAdapter.createFromResource(user_Registration.this,R.array.usertype, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
-        spinner.setAdapter(userType);
+        // Getting attachment of the filed layout
+        profile_pic = findViewById(R.id.profile_pic);
+        add_image = findViewById(R.id.add_image);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
 
-        add_image.setOnClickListener(view -> {
-            ImagePicker.Companion.with(this)
-                    .crop()
-                    .compress(1024)
-                    .maxResultSize(512,512)
-                    .start();
-        });
+
+        add_image = findViewById(R.id.add_image);
+
+         Button Register = findViewById(R.id.register);
+         Register.setOnClickListener(view -> {
+
+             if(uri != null) {
+                 registerFirebase(uri);
+             }
+         });
+
+
+
+
+        add_image.setOnClickListener(view -> ImagePicker.Companion.with(user_Registration.this)
+                     .cropSquare()
+                     .compress(1024)
+                     .maxResultSize(1080,1080)
+                     .start(10));
+
 
 
 
@@ -44,7 +76,85 @@ public class user_Registration extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Uri uri = Objects.requireNonNull(data).getData();
-        profile_pic.setImageURI(uri);
+        if(requestCode == 10) {
+            assert data != null;
+             uri =  data.getData();
+            assert uri != null;
+            Toast.makeText(this, ""+uri, Toast.LENGTH_SHORT).show();
+            Picasso.get()
+                    .load(uri)
+                    .centerCrop()
+                    .transform(new CircleTransform())
+                    .resize(200,200)
+                    .into(profile_pic);
+
+            profile_pic.setImageURI(uri);
+
+
+        }
+
     }
+
+    public void registerFirebase(Uri uri){
+
+        Full_name = findViewById(R.id.Full_name);
+        Email = findViewById(R.id.email);
+        Phone = findViewById(R.id.Phone);
+        Password = findViewById(R.id.password);
+        Confirm_password = findViewById(R.id.confirm_password);
+        spinner = findViewById(R.id.spinner);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("User");
+        arrayList.add("Food-Offer");
+        ArrayAdapter<String> userType = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, arrayList);
+        spinner.setAdapter(userType);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String tutorialsName = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), "Selected: " + tutorialsName,          Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView <?> parent) {
+            }
+        });
+        StorageReference image_upload = FirebaseStorage.getInstance().getReference("Image");
+        StorageReference Fileref = image_upload.child(System.currentTimeMillis()+"."+getFile(uri));
+        Fileref.putFile(uri)
+                .addOnSuccessListener(taskSnapshot -> Fileref.getDownloadUrl().addOnSuccessListener(uri1 -> {
+                    UserHolder userHolder = new UserHolder(Full_name.getText().toString(),Email.getText().toString(),Phone.getText().toString(),Password.getText().toString(), uri1.toString(), spinner.getSelectedItem().toString());
+                    String userId = databaseReference.push().getKey();
+                    if(spinner.getSelectedItem().toString().equals("User")){
+                        assert userId != null;
+                        databaseReference.child("user").child(userId).setValue(userHolder);
+                    progressBar.setVisibility(View.INVISIBLE);
+                    Toast.makeText(user_Registration.this, "Success", Toast.LENGTH_SHORT).show();
+                     }
+                    else if(spinner.getSelectedItem().toString().equals("Food-Offer")){
+                        assert userId != null;
+                        databaseReference.child("food-offer").child(userId).setValue(userHolder);
+                        progressBar.setVisibility(View.INVISIBLE);
+                        Toast.makeText(user_Registration.this, "Success", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(user_Registration.this, "Food Offer not made", Toast.LENGTH_SHORT).show();
+                    }
+                }))
+                .addOnProgressListener(snapshot -> progressBar.setVisibility(View.VISIBLE))
+                .addOnFailureListener(e -> {
+                  progressBar.setVisibility(View.INVISIBLE);
+                    Toast.makeText(user_Registration.this, "Failed", Toast.LENGTH_SHORT).show();
+                });
+
+    }
+
+   private String getFile(Uri mUri){
+       ContentResolver cr = getContentResolver();
+       MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+       return mimeTypeMap.getExtensionFromMimeType(cr.getType(mUri));
+   }
+
+
+
+
 }
