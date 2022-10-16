a2af96acdd9e73825a5fd6d8ba022844f3714a39
abhik <abhikchowdhury.20@nshm.edu.in>
2022-10-16 11:47:09 +0530

    This app will going help people who want to help others
    Update on 23sept with two new activity
    new on 24th splash gone
    CircleTransform new java class to make the image circular
    Firebase Two branch again
    user type visibility
    Loading Screen

---
+++ app/src/main/java/com/example/ufood/Loading_Screen.java
@@ -0,0 +1,29 @@
+package com.example.ufood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class Loading_Screen extends AppCompatActivity {
+    View nContext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+        nContext = findViewById(R.id.backgroundLayout);
+        nContext.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(Loading_Screen.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        },4000);
+    }
+}
